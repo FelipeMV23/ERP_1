0e3201608c1d1ffc9c6589eee33da037d71fbe41
CristopherGodoy <c.godoylefiman@gmail.com>
2025-06-04 18:34:22 -0400

    view

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ProductoForm
 # Create your views here.
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')  # Cambia esta ruta si tienes otra URL de destino
+    else:
+        form = ProductoForm()
+    
+    return render(request, 'productos/agregar_producto.html', {'form': form})

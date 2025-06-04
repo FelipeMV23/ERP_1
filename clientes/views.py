82e4e34359455cfd642f6e9306549742f16a9d63
= <=>
2025-06-04 18:40:34 -0400

    Ajustes en app clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from clientes.forms import ClienteForm
 
-# Create your views here.
+
+def crear_cliente(request):
+    if request.method =='POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('consultar_clientes')
+    
+    else:
+        form = ClienteForm()
+    
+    return(render(request, 'clientes/crear_cliente.html', {'form':form}))

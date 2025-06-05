992f957de17613c5a1adb38a7832583e1d2d1075
CristopherGodoy <c.godoylefiman@gmail.com>
2025-06-05 14:12:11 -0400

    carpeta static con algunos css

--- ERP/urls.py
+++ ERP/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
     path('productos/', include('productos.urls'))
-    
+    path('pedidos/', include('pedidos.urls')),
 ]

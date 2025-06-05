b9e81a621491c3991f2f046b6c413dc699b0ed9d
CristopherGodoy <c.godoylefiman@gmail.com>
2025-06-04 20:48:46 -0400

    editar con bootstrap

--- productos/forms.py
+++ productos/forms.py
@@ -4,30 +4,15 @@ from .models import Producto
 from django import forms
 from .models import Producto
 
+from django import forms
+from .models import Producto
+
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = [
-            'id_producto',
-            'nombre_producto',
-            'tipo_material',  # se renderiza como <select>
-            'tamaño',         # se renderiza como <select>
-            'color',          # se renderiza como <select>
-            'descripcion',
-            'stock',
-            'precio_unitario',
-        ]
-        widgets = {
-            'descripcion': forms.Textarea(attrs={'rows': 3}),
-        }
+        fields = '__all__'
 
-        labels = {
-            'id_producto': 'Código del producto',
-            'nombre_producto': 'Nombre',
-            'tipo_material': 'Tipo de material',
-            'tamaño': 'Tamaño',
-            'color': 'Color',
-            'descripcion': 'Descripción',
-            'stock': 'Stock disponible',
-            'precio_unitario': 'Precio unitario ($)',
-        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
